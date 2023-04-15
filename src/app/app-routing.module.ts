bd62c6a70b7286cd9b32fb80a74c5a1384e21da9
clifford-andrew-stevenson <clifford.andrew.stevenson@gmail.com>
2023-04-15 00:16:34 -0700

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomesComponent } from './homes/homes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // This sets up the default route to the homes component
+  // created homes component with route
+  {
+    path: 'homes',
+    component: HomesComponent
+  },
+  // redirect to /homes
+  {
+    path: '',
+    redirectTo: '/homes',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
